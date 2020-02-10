98e003b5e92016431babb357507834138760cf0f
Nicolas Germain <nicolas.germain@tbs-sct.gc.ca>
2020-02-10 10:46:55 -0500

    added list unpacking, converted input to scrape function as list and fixed extra spaces in input

--- legal_title_reg.py
+++ legal_title_reg.py
@@ -2,7 +2,6 @@ from lxml import html
 import requests
 import pandas as pd
 
-
 #create an empty list to store the data
 versions = []
 
@@ -14,15 +13,12 @@ with open('faa_registry_names.txt','r') as fp:
         line = fp.readline()
 
         
-def scrape(schedule):
+def scrape(schedules):
     dept_dict = {}
     dept_counter = 0
     for version in versions :
-        if version[0] in schedule:
-            version_start_date = version[1]
-            version_end_date = version[2]
-            url = version[3]
-
+        schedule, version_start_date, version_end_date, url = version
+        if schedule in schedules:
             print(url)
             page = requests.get(url)
             print(page.status_code)
@@ -30,9 +26,9 @@ def scrape(schedule):
 
             version_depts_en = tree.xpath('//*/p[@class="BilingualItemFirst"]/text()')
             version_depts_fr = tree.xpath('//*/p[@class="BilingualItemSecond"]/text()')
-            version_depts_biling = list(zip(version_depts_en,version_depts_fr))
+            version_depts_bilingual = list(zip(version_depts_en,version_depts_fr))
 
-            for dept in version_depts_biling:
+            for dept in version_depts_bilingual:
                 dept_name = dept[0]
                 if dept_name in dept_dict:
                     #if the department already exists, extend the end date
@@ -41,7 +37,7 @@ def scrape(schedule):
                     #if this is the first time we see this name, add the department
                     dept_counter += 1
                     dept_dict[dept_name] = [dept_counter,         #id
-                                            version[0],           #schedule
+                                            schedule,             #schedule
                                             dept_name,            #name en 
                                             dept[1],              #name fr
                                             version_start_date,   #start date
@@ -50,7 +46,7 @@ def scrape(schedule):
     all_final_depts = pd.DataFrame.from_dict(dept_dict)
     all_final_depts = all_final_depts.T
     all_final_depts.columns = ['id','schedule','title_en','title_fr','start','end']
-    all_final_depts.to_csv('schedules ' + schedule + '.csv', index=False)
+    all_final_depts.to_csv('schedules ' + ','.join(schedules) + '.csv', index=False)
 
-scrape('1,1.1,2,3')
-scrape('1,4,5')
+scrape(['1','1.1','2','3'])  #financial departments
+scrape(['1','4','5'])        #HR departments
